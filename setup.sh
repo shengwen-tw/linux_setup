98aeb9e515d9cee189f4af4db9fc9336446565fc
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-02-15 12:42:32 +0800

    Add fxtix-chewing and kicad

--- setup.sh
+++ setup.sh
@@ -5,13 +5,16 @@
 #Add apt repo
 sudo add-apt-repository ppa:numix/ppa
 sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
+sudo add-apt-repository ppa:fcitx-team
+sudo add-apt-repository --yes ppa:js-reynaud/kicad-4
 sudo apt-get update
 
 #Install apt
 sudo apt-get install numix-icon-theme-circle #Circular dock
 sudo apt-get install elementary-tweaks #Elementary os tweeks tool
 sudo apt-get install bumblebee bumblebee-nvidia primus linux-headers-generic #nvidia third party hardware driver
-sudo apt-get install vim git tig gnome-terminal gedit nautilus pv bittorrent
+sudo apt-get install im-config fcitx fcitx-config-gtk fcitx-chewing #fctix-chewing
+sudo apt-get install vim git tig gnome-terminal gedit nautilus pv bittorrent kicad
 
 #Gnome theme [https://github.com/denysdovhan/one-gnome-terminal]
 wget https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh && sudo bash ./one-dark.sh
